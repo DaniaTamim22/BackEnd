4b313c3c8b4eae3d296dbb8cbf6bab1bcbc7c5db
Dania Tamim <dania.tamim@live.se>
2022-06-17 14:17:22 +0200

    Lekton-3

--- React/Lektion-3/2-useeffect/src/App.js
+++ React/Lektion-3/2-useeffect/src/App.js
@@ -1,9 +1,57 @@
+import { useState, useEffect, useCallback } from 'react';
+import axios from 'axios'
+import TodoItem from './components/TodoItem';
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState([
+    // {
+    //   id: 1,
+    //   title: 'todo 1',
+    //   completed: false,
+    //   user: 1
+    // },
+    // {
+    //   id: 2,
+    //   title: 'todo 2',
+    //   completed: false,
+    //   user: 1
+    // }
+  ])
+
+  const [url] = useState(' http://localhost:8080/todos')
+
+
+  // const getTodos = async () => {
+  //   const res = await axios.get(url)
+  //   setTodos(res.data)
+
+  // }
+
+  const getTodos = useCallback(async () => {
+    const res = await axios.get(url)
+    setTodos(res.data)
+
+
+  }, [url])
+  useEffect(() => {
+    // axios.get(url)
+    //   .then(res => {
+    //     setTodos(res.data)
+    //   })
+    getTodos()
+
+  }, [getTodos])
   return (
-    <div className="App">
-      app
+    <div className="App container">
+      {
+        todos.map(todo => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))
+      }
+      <button className='btn'>All users</button>
+      <button className='btn'>user 1</button>
     </div>
   );
 }
---
+++ React/Lektion-3/2-useeffect/src/components/TodoItem.jsx
@@ -0,0 +1,10 @@
+const TodoItem = ({ todo }) => {
+	return (
+		<div className="todo-item">
+			<h2>{todo.title}</h2>
+			<button className="btn">X</button>
+		</div>
+	);
+};
+
+export default TodoItem;
